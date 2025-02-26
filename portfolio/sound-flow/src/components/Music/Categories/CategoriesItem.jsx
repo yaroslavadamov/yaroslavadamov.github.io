34f4d8e9771b1fa061ecad79bd7b11b94ebb8ee7
Yaroslav Adamov <adamovyaroslav@gmail.com>
2025-02-26 12:17:06 +0300

    SoundFlow: add CategoriesList autoscrolling

--- portfolio/sound-flow/src/components/Music/Categories/CategoriesItem.jsx
+++ portfolio/sound-flow/src/components/Music/Categories/CategoriesItem.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react';
 import './CategoriesItem.scss';
 
 export default function CategoriesItem({index, name, activePlaylist, setActivePlaylist}) {
@@ -5,11 +6,24 @@ export default function CategoriesItem({index, name, activePlaylist, setActivePl
 		setActivePlaylist(index);
 	}
 
+	const buttonRef = useRef(null);
+
+	useEffect(() => {
+		buttonRef.current.addEventListener('click', () => {
+			buttonRef.current.scrollIntoView({
+				behavior: 'smooth',
+				block: 'nearest',
+				inline: 'center'
+			});
+		})
+	});
+
 	return(
 		<li className={"music__categories-item" + ((index === activePlaylist) ? " is-active" : "")}>
 			<button
 				className="h2 secondary adaptive"
 				onClick={handleClick}
+				ref={buttonRef}
 			>
 				{name}
 			</button>
